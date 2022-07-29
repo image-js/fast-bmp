007471f112ab7611e5168f5564a4b014f3052124
Daniel Kostro <kostro.d@gmail.com>
2022-07-29 15:32:32 +0200

    chore: update deps

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 
 const Encoder = require('./BMPEncoder');
 
-exports.encode = function (data) {
-    const encoder = new Encoder(data);
-    return encoder.encode();
+exports.encode = function encode(data) {
+  const encoder = new Encoder(data);
+  return encoder.encode();
 };
-
